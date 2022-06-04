e484824dc8c5fdf40d6b30fc56b0df3882168e9f
Geissel Espino <geisselespino05@outlook.com>
2022-06-04 14:37:48 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+int main(){
+    float a;
+    cout<<"Enter your number: "<<endl;
+    cin>>a;
+    cout<<"Your number is: "<<a<<endl;
+    system("PAUSE");
+}
